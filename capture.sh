afe31cb1c55073ab76d47a01fbe43c675ca161dd
Andranik Voskanyan <voskanyan069@gmail.com>
2022-09-04 20:43:02 +0100

    Added images directory

--- capture.sh
+++ capture.sh
@@ -8,15 +8,18 @@ fi
 
 TIMEOUT=$1
 COUNT=$2
+IMGDIR="./images"
 echo "Timeout set to $TIMEOUT sec"
 echo "Count of captures set to $COUNT"
+echo "Captures will be saved in $IMGDIR"
 
 capture() {
-    FILENAME="./img_$1.jpg"
+    FILENAME="$IMGDIR/img_$1.jpg"
     raspistill -o $FILENAME
     echo "Capture output saved to $FILENAME"
 }
 
+mkdir -p images/
 for (( i=0; i<$COUNT; ++i ))
 do
     capture $i
